3501c1e46cc6a78a72f3c3d45c593e4cd9fee035
JaeHoon Lee <jaehoonlee88@gmail.com>
2020-09-24 21:14:53 -0700

    Add training stage.

--- vision/MNIST/mnist.py
+++ vision/MNIST/mnist.py
@@ -8,12 +8,17 @@ import torchvision.transforms as transforms
 
 num_workers = 0
 batch_size = 20
+valid_size = 0.2
 
 # Loading Data
 transform = transforms.ToTensor()
 
 train_data = datasets.MNIST(root='data', train=True, download=True, transform=transform)
 test_data = datasets.MNIST(root='data', train=False, download=True, transform=transform)
+
+num_train = len(train_data)
+indices = list(range(num_train))
+np.random.shuffle(indices)
 
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, num_workers=num_workers)
 test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, num_workers=num_workers)
@@ -38,3 +43,20 @@ optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
 
 # number of epochs to train the model
+n_epochs = 30
+
+model.train()
+
+for epoch in range(n_epochs):
+    train_loss = 0.0
+
+    for data, target in train_loader:
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item() * data.size(0)# Since loss.item() have mean of batch, multiple batch size
+
+    train_loss = train_loss/len(train_loader.dataset)# Average training loss
+    print('Epoch: {} \tTraining Loss: {:.6f}'.format(epoch+1, train_loss))
