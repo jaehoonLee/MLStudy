d290172f6cb92fe4aee47ef3c858d06760c5397f
JaeHoon Lee <jaehoonlee88@gmail.com>
2020-09-29 23:49:35 -0700

    Add gray image loading part

--- vision/CNN/layer_visualization.py
+++ vision/CNN/layer_visualization.py
@@ -7,5 +7,6 @@ bgr_image = cv2.imread(img_path)
 gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
 
 gray_image = gray_image.astype("float32")/255
+
 plt.imshow(gray_image, cmap='gray')
-plt.show()
+plt.savefig('data/seattle_result.png')
